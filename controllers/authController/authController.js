275d8e531c367151bba88b33654d01b15423ce8d
dario <darioasaro29@gmail.com>
2020-03-16 12:12:43 -0300

    udpate integracion

--- controllers/authController/authController.js
+++ controllers/authController/authController.js
@@ -34,7 +34,6 @@ exports.login = (req, res) => {
 
 //----Crea el usuario en la base de datos con la contraseña encriptada
 exports.register = (req, res) => {
-  console.log('user',req.body)
   const username = req.body.username;
   const password = req.body.password;
   const rol = 1;
