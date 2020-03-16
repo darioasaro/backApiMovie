87b8f07d292abb753aee66c79ab81d8e90f6c96c
Jazmin <jaabris@hotmail.com>
2020-03-16 11:18:42 -0300

    User DAO modificado

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER ,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME 
+  database: process.env.DB_NAME
 });
 
 connection.connect(err => {
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,101 +1,116 @@
-const daoMovie = require('../dao/movieDao.js')
-exports.index = (req, res )=>{
-    daoMovie.getAll((err, movies) => {
-        res.json({'movies' : movies});
-    } )
-}
+const daoMovie = require("../dao/movieDao.js");
+exports.index = (req, res) => {
+  daoMovie.getAll((err, movies) => {
+    res.json({ movies: movies });
+  });
+};
 
-exports.store = (req, res)=>{
-    let {id_api, original_title, backdrop_path, poster_path, overview, vote_average, vote_count} = req.body;
-    let created = time();
-    try{
-        if(isExistMovie(id_api)){
-            movie1 = {
-                id_api: id_api,
-                original_title : original_title,
-                backdrop_path : backdrop_path,
-                poster_path : poster_path,
-                overview : overview,
-                vote_average : vote_average,
-                vote_count : vote_count,
-                created_at : created,
-                updated_at : created
-            }
-            daoMovie.createMovie(movie1)
-            res.json('Se agregó correctamente la pelicula')
-        }else{
-            res.status(200).json('La pelicula ya existe!!')
-        }
-    }catch(e){
-        console.log(e);
-    }    
-}
-exports.edit= (req, res)=>{
-    let movie;    
-    daoMovie.getMovie(req.params.id,(err,cb)=>{
-        if (err){
-            res.status(500).json({'result':false, 'menssage': 'internal error'})
-        }else{
-            movie = cb; 
-            console.log(movie);
-            res.json({'movie':movie})
-                       
-        }
-    })
-    
-}
-exports.update = (req, res)=>{    
-    let {id_api, original_title, backdrop_path, poster_path, overview, vote_average, vote_count} = req.body;
-    let update = time();
-    try{
-        movie = {
-            id_api: id_api,
-            original_title : original_title,
-            backdrop_path : backdrop_path,
-            poster_path : poster_path,
-            overview : overview,
-            vote_average : vote_average,
-            vote_count : vote_count,
-            updated_at : update
-        }
-        daoMovie.updateMovie(movie, req.params.id)
-        res.json('Se editó correctamente la pelicula')
-    }catch(e){
-        console.log(e);
+exports.store = (req, res) => {
+  let {
+    id_api,
+    original_title,
+    backdrop_path,
+    poster_path,
+    overview,
+    vote_average,
+    vote_count
+  } = req.body;
+  let created = time();
+  try {
+    if (!isExistMovie(id_api)) {
+      movie1 = {
+        id_api: id_api,
+        original_title: original_title,
+        backdrop_path: backdrop_path,
+        poster_path: poster_path,
+        overview: overview,
+        vote_average: vote_average,
+        vote_count: vote_count,
+        created_at: created,
+        updated_at: created
+      };
+      daoMovie.createMovie(movie1);
+      res.json("Se agregó correctamente la pelicula");
+    } else {
+      res.status(200).json("La pelicula ya existe!!");
     }
-}
-exports.delete= (req, res)=>{
-    let deleted_at = time()
-    try{
-        daoMovie.deleteMovie(req.params.id, deleted_at)
-        res.json('Se elimino correcamente')
-    }catch (e){
-        console.log(e);        
-    }
-}
-
-isExistMovie = (id_api,res)=>{
-    let exist = false
-    let movie;
-    daoMovie.isExist(id_api, (err,cb)=>{
-        if (err){
-            res.status(500).json({'result':false, 'menssage': 'internal error'})
-        }else{
-            movie = cb;            
-        }
-    })    
-    if(movie){
-        exist = true
-        return exist
-    }else{
-        return exist
-    } 
-}
-
-time =()=>{
-    let hoy = new Date();
-    var fecha = hoy.getFullYear() + '-'+ (hoy.getMonth()+1) +'-'+ hoy.getDate();
-    var hora = hoy.getHours()+':'+ hoy.getMinutes()+':'+hoy.getSeconds();
-    var created = fecha + ' ' + hora
-    return created;
+  } catch (e) {
+    console.log(e);
   }
+};
+exports.edit = (req, res) => {
+  let movie;
+  daoMovie.getMovie(req.params.id, (err, cb) => {
+    if (err) {
+      res.status(500).json({ result: false, menssage: "internal error" });
+    } else {
+      movie = cb;
+      console.log(movie);
+      res.json({ movie: movie });
+    }
+  });
+};
+exports.update = (req, res) => {
+  let {
+    id_api,
+    original_title,
+    backdrop_path,
+    poster_path,
+    overview,
+    vote_average,
+    vote_count
+  } = req.body;
+  let update = time();
+  try {
+    movie = {
+      id_api: id_api,
+      original_title: original_title,
+      backdrop_path: backdrop_path,
+      poster_path: poster_path,
+      overview: overview,
+      vote_average: vote_average,
+      vote_count: vote_count,
+      updated_at: update
+    };
+    daoMovie.updateMovie(movie, req.params.id);
+    res.json("Se editó correctamente la pelicula");
+  } catch (e) {
+    console.log(e);
+  }
+};
+exports.delete = (req, res) => {
+  let deleted_at = time();
+  try {
+    daoMovie.deleteMovie(req.params.id, deleted_at);
+    res.json("Se elimino correcamente");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+isExistMovie = (id_api, res) => {
+  let exist = false;
+  let movie;
+  daoMovie.isExist(id_api, (err, cb) => {
+    if (err) {
+      res.status(500).json({ result: false, menssage: "internal error" });
+    } else {
+      movie = cb;
+    }
+  });
+  if (movie) {
+    exist = true;
+    return exist;
+  } else {
+    return exist;
+  }
+};
+
+time = () => {
+  let hoy = new Date();
+  var fecha =
+    hoy.getFullYear() + "-" + (hoy.getMonth() + 1) + "-" + hoy.getDate();
+  var hora = hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds();
+  var created = fecha + " " + hora;
+  return created;
+};
