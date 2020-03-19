cf3053517b2220d292392da30342deeb97167647
MauroYini <mauro8792yini@gmail.com>
2020-03-19 10:34:34 -0300

    archivo de conf

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER || "dario",
-  password: process.env.DB_PASSWORD || "1234",
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME || "apibackmovie"
 });
 
